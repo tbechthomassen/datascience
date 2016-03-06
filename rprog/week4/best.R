a09c44419c17b7215ef2a258d499f24024fb59e1
Thomas Bech-Thomassen <thomas.bech.thomassen@gmail.com>
2016-03-06 21:05:53 +0100

    functionalised errythin'

--- rprog/week4/best.R
+++ rprog/week4/best.R
@@ -1,42 +1,88 @@
-best <- function(state, outcome) {
-      outData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-      if(state %in% outData$State & outcome %in% outData$){
-            
-      }
-      
-      ## Return hospital name in that state with lowest 30-day death rate
+best <- function(inputState, outcome) {
+     
+     ## get outcome data
+     alldata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+     outData <- alldata[, c(2, 7, 11, 17, 23)]
+     colnames(outData) <- c("Hospital.name", "state", "heart.attack", "heart.failure", "pneumonia")
+     outcomeList <- c("heart failure", "heart attack", "pneumonia")
+     
+     
+     ## check if variables are in dataset
+     
+     if(inputState %in% outData$state & outcome %in% outcomeList){
+          
+          if (outcome == "heart failure"){
+               stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.failure"))
+               attach(stateData)
+               sorted <- stateData[order(heart.failure, Hospital.name),]
+               sorted[1,"Hospital.name"]
+               # detach(stateData)
+               # rm(stateData)
+          } else if (outcome == "heart attack"){
+               stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.attack"))
+               attach(stateData)
+               sorted <- stateData[order(heart.attack, Hospital.name),]
+               sorted[1,"Hospital.name"]
+               # detach(stateData)
+               # rm(stateData)
+          } else if (outcome == "pneumonia"){
+               stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "pneumonia"))
+               attach(stateData)
+               sorted <- with(stateData, stateData[order(pneumonia, Hospital.name),])
+               sorted[1,"Hospital.name"]
+               detach(stateData)
+               rm(stateData)
+          } else {stop("'ALT ER GALT -- IGEN!'")}
+          
+     } else if(!is.element(inputState, outData$state)){
+          stop("'invalid state'")
+          
+     } else if(!is.element(outcome, outcomeList)){
+          stop("'invalid outcome'")
+          
+     } else{stop('"ALT ER GALT!"')}
 }
 
-## set variables
-inputState <- "AL" # two-letter abbreviation of State
-outcome <- "heart failure" # corresponds to a value in the outcomeList
+# ## set variables
+# inputState <- "AL" # two-letter abbreviation of State
+# outcome <- "heart failure" # corresponds to a value in the outcomeList
 
 
-## get outcome data
-alldata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-outData <- alldata[, c(2, 7, 11, 17, 23)]
-colnames(outData) <- c("Hospital.name", "state", "heart.attack.30mort", "heart.failure.30mort", "pneumonia.30mort")
-outcomeList <- c("heart failure", "heart attack", "pneumonia")
-
-
-## check if variables are in dataset
-
-if(inputState %in% outData$state & outcome %in% outcomeList){
-      
-      if (outcome == "heart failure"){
-            stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.failure.30mort"))
-      } else if (outcome == "heart attack"){
-            stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.attack.30mort"))
-      } else if (outcome == "pneumonia"){
-            stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "pneumonia.30mort"))
-      } else {stop("'ALT ER GALT -- IGEN!'")}
-      #       
-      #       sorted <- with(stateData, order("pneumonia"))
-      
-} else if(!is.element(inputState, outData$state)){
-      stop("'invalid state'")
-} else if(!is.element(outcome, outcomeList)){
-      stop("'invalid outcome'")
-} else{stop('"ALT ER GALT!"')}
-
-
+# ## get outcome data
+# alldata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+# outData <- alldata[, c(2, 7, 11, 17, 23)]
+# colnames(outData) <- c("Hospital.name", "state", "heart.attack", "heart.failure", "pneumonia")
+# outcomeList <- c("heart failure", "heart attack", "pneumonia")
+# 
+# 
+# ## check if variables are in dataset
+# 
+# if(inputState %in% outData$state & outcome %in% outcomeList){
+#      
+#      if (outcome == "heart failure"){
+#           stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.failure"))
+#           attach(stateData)
+#           sorted <- stateData[order(heart.failure, Hospital.name),]
+#           sorted[1,"Hospital.name"]
+#      } else if (outcome == "heart attack"){
+#           stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "heart.attack"))
+#           attach(stateData)
+#           sorted <- stateData[order(heart.attack, Hospital.name),]
+#           sorted[1,"Hospital.name"]
+#           
+#      } else if (outcome == "pneumonia"){
+#           stateData <- subset(outData, subset = state == inputState, select = c("Hospital.name", "state", "pneumonia"))
+#           attach(stateData)
+#           sorted <- stateData[order(pneumonia, Hospital.name),]
+#           sorted[1,"Hospital.name"]
+#      } else {stop("'ALT ER GALT -- IGEN!'")}
+#      
+# } else if(!is.element(inputState, outData$state)){
+#      stop("'invalid state'")
+#      
+# } else if(!is.element(outcome, outcomeList)){
+#      stop("'invalid outcome'")
+#      
+# } else{stop('"ALT ER GALT!"')}
+# 
+# 
